b91562c940f9995c830d457a2b6f0a8fd6d9c4e7
Soloparame <yihenewrebika@gmail.com>
2025-10-26 03:05:55 -0700

    now

--- src/components/Header/ThemeToggler.tsx
+++ src/components/Header/ThemeToggler.tsx
@@ -1,9 +1,25 @@
 import { useTheme } from "next-themes";
+import { useEffect, useState } from "react";
 
 const ThemeToggler = () => {
   const { theme, setTheme } = useTheme();
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  if (!mounted) {
+    return (
+      <div className="flex h-9 w-9 items-center justify-center rounded-full bg-gray-2 md:h-14 md:w-14">
+        <div className="h-5 w-5"></div>
+      </div>
+    );
+  }
+
   return (
-    <button aria-label='theme toggler'
+    <button 
+      aria-label='theme toggler'
       onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
       className="flex items-center justify-center text-black rounded-full cursor-pointer bg-gray-2 dark:bg-dark-bg h-9 w-9 dark:text-white md:h-14 md:w-14"
     >
